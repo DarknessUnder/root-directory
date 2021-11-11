4d5997e49ff990b91795e500373a4f2d7600a078
tant2 <tant2@rpi.edu>
2021-11-11 13:25:30 -0500

    slide nav bar and canlender

--- docs/js/index.js
+++ docs/js/index.js
@@ -1,8 +1,30 @@
 $(function () {
+    var state = true;
     $("#navBLeft").load("nav.html");
     $(".pageLink").click(function(){
         var link = "" + $(this).text().toLowerCase() + ".html";
         window.location.href=link;
     });
+
+    $(".landing-page").click(function () {
+        if(state){
+            $("#navLeft").remove();
+            $("#navBLeft").load("navEmpty.html");
+            state = false;
+        }
+        else{
+            state = true;
+        }
+    });
+
+    $("#navBLeft").click(function () {
+        if(!state){
+            $("#navEmpty").remove();
+            $("#navBLeft").load("nav.html");
+            
+        }
+        state = false;
+    });
     
   });
+
