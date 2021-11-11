e1a6f9f4ff571de2119ea4073987c9c6be537faf
tant2 <tant2@rpi.edu>
2021-11-11 13:58:16 -0500

    rpi-tv dummy pagge

--- docs/js/demo.js
+++ docs/js/demo.js
@@ -1,5 +1,5 @@
 var events = [
-  {'Date': new Date(2021, 10, 11), 'Title': 'SDD Demo'},
+  {'Date': new Date(2021, 10, 11), 'Title': 'SDD Demo', 'Link': 'index.html'},
   {'Date': new Date(2021, 10, 12), 'Title': 'SDD Beta Release', 'Link': 'https://docs.google.com/document/d/1aDV_G4XrcJIoWNtrpYsaJop_2_M69id6TD-jsQlJtSA'},
   {'Date': new Date(2021, 10, 27), 'Title': 'Exam', 'Link': 'csci-4440.html'},
 ];
