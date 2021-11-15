33eb944b9bc458e4b6acb27bc5f6ead48df197e7
tant2 <tant2@rpi.edu>
2021-11-15 02:28:14 -0500

    repair docs

---
+++ docs/js/demo.js
@@ -0,0 +1,10 @@
+//demo inputs
+
+var events = [
+  {'Date': new Date(2021, 10, 11), 'Title': 'SDD Demo', 'Link': 'index.html'},
+  {'Date': new Date(2021, 10, 12), 'Title': 'SDD Beta Release', 'Link': 'https://docs.google.com/document/d/1aDV_G4XrcJIoWNtrpYsaJop_2_M69id6TD-jsQlJtSA'},
+  {'Date': new Date(2021, 10, 27), 'Title': 'Exam', 'Link': 'csci-4440.html'},
+];
+var settings = {};
+var element = document.getElementById('caleandar');
+caleandar(element, events, settings);
---
+++ docs/js/index.js
@@ -0,0 +1,49 @@
+$(function () {
+
+    //load navigation bar
+    var state = true;
+    $("#navBLeft").load("nav.html");
+    $(".pageLink").click(function(){
+        var link = "" + $(this).text().toLowerCase() + ".html";
+        window.location.href=link;
+    });
+
+
+    //hide navigation bar
+    $(".landing-page").click(function () {
+        if(state){
+            $("#navLeft").remove();
+            $("#navBLeft").load("navEmpty.html");
+            state = false;
+        }
+        else{
+            state = true;
+        }
+    });
+
+    $(".profile-page").click(function () {
+        if(state){
+            $("#navLeft").remove();
+            $("#navBLeft").load("navEmpty.html");
+            state = false;
+        }
+        else{
+            state = true;
+        }
+    });
+
+//display navigation bar
+    $("#navBLeft").click(function () {
+        if(!state){
+            $("#navEmpty").remove();
+            $("#navBLeft").load("nav.html");
+            
+        }
+        state = false;
+    });
+
+   
+
+    
+  });
+
