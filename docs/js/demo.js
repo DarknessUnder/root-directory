bdc035c86e7374dbe828f0b06e6dea2cde7bc472
tant2 <tant2@rpi.edu>
2021-12-06 20:17:43 -0500

    quick changes

--- docs/js/demo.js
+++ docs/js/demo.js
@@ -1,9 +1,20 @@
 //demo inputs
 
-var events = [
-  {'Date': new Date(2021, 10, 11), 'Title': 'SDD Demo', 'Link': 'index.html'},
-  {'Date': new Date(2021, 10, 12), 'Title': 'SDD Beta Release', 'Link': 'https://docs.google.com/document/d/1aDV_G4XrcJIoWNtrpYsaJop_2_M69id6TD-jsQlJtSA'},
-  {'Date': new Date(2021, 10, 27), 'Title': 'Exam', 'Link': 'csci-4440.html'},
+var events = [{
+    'Date': new Date(2021, 10, 11),
+    'Title': 'SDD Demo',
+    'Link': 'index.html'
+  },
+  {
+    'Date': new Date(2021, 10, 12),
+    'Title': 'SDD Beta Release',
+    'Link': 'https://docs.google.com/document/d/1aDV_G4XrcJIoWNtrpYsaJop_2_M69id6TD-jsQlJtSA'
+  },
+  {
+    'Date': new Date(2021, 10, 27),
+    'Title': 'Exam',
+    'Link': 'csci-4440.html'
+  },
 ];
 var settings = {};
 var element = document.getElementById('caleandar');
@@ -11,19 +22,27 @@ caleandar(element, events, settings);
 
 $(function () {
   function addEvent() {
-    var tempElement = {'Date': new Date(2021, 10, 22), 'Title': 'SDD Beta Demo', 'Link': 'index.html'};
+    var tempElement = {
+      'Date': new Date(2021, 10, 22),
+      'Title': 'SDD Beta Demo',
+      'Link': 'index.html'
+    };
     events.push(tempElement);
     $("#caleandar").empty();
     caleandar(element, events, settings);
-  };
+  }
   addEvent();
 
 
-  $('#newEvent').submit(function(event) {
+  $('#newEvent').submit(function (event) {
 
     var tempNew = $('#newEvent').serializeArray();
 
-    var tempElement = {'Date': new Date(tempNew[2].value, tempNew[3].value -1, tempNew[4].value), 'Title': tempNew[0].value, 'Link': tempNew[1].value};
+    var tempElement = {
+      'Date': new Date(tempNew[2].value, tempNew[3].value - 1, tempNew[4].value),
+      'Title': tempNew[0].value,
+      'Link': tempNew[1].value
+    };
     events.push(tempElement);
     $("#caleandar").empty();
     caleandar(element, events, settings);
@@ -33,4 +52,4 @@ $(function () {
     $('#ndate').val(2);
     event.preventDefault();
   });
-})
+});
